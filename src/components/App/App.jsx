406e1cf5bdb01034348a33f2a2dc52a4f90948e2
Annastasia-Seymour <stasia1seymour@gmail.com>
2024-10-16 21:34:14 +0200

    Planning on implementing my routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css'; // Include your styles
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import { BrowserRouter ,Routes, Router , Route } from 'react-router-dom';
+import { BrowserRouter ,Routes, Router , Route } from 'react-router-dom';
 //import data from '../DataFetch/FetchData.jsx'; // i have no idea how its still working 
 import Header from '../Header/Header.jsx';
 import LandingPage from '../../pages/LandingPage/landingPage.jsx';
+import ShowAllPreview from '../../pages/Show All/ShowAll.jsx';
+import FetchAllData from '../DataFetch/FetchAllData.jsx';
 
 //remember we want to make like a filter modal to pop up  when the genre is selected
 
@@ -12,11 +14,19 @@ import LandingPage from '../../pages/LandingPage/landingPage.jsx';
 
 function App() {
   return (
-    <>
-      <Header />
-    <LandingPage />
+    <BrowserRouter>
+       <Header />
+        <Routes>
+          <Route path='/' element = { <LandingPage/>}/>
+          <Route path='/show-all' element { <ShowAllPreview/>} />
+
+        </Routes>
     
-    </>
+    
+    </BrowserRouter>
+
+    
+    
   
   );
 }
